d092d1f27c86d6ac91d071d45b5e4fc555e8a095
Taity-mini <taitymini@hotmail.co.uk>
2016-08-31 15:40:14 +0100

    Change Links to localhost

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -1,5 +1,5 @@
 <header>
-   <a href="http://bucktest-rgu.azurewebsites.net/index.php" class="nohover">
+   <a href="http://localhost/bucktest/index.php" class="nohover">
       <div class="banner">      
          <h1 class="middle centre">Bucksburn Amateur Swimming Club</h1>      
       </div>
@@ -13,79 +13,79 @@
             <ul>
                <li class="divider"></li>
                <li class="has-dropdown not-click">
-                  <a href="http://bucktest-rgu.azurewebsites.net/galas.php" role="link">Galas</a> 
+                  <a href="http://localhost/bucktest/galas.php" role="link">Galas</a> 
                   <ul class="dropdown">
-                     <li><a href="http://bucktest-rgu.azurewebsites.net/upcoming-galas.php" role="link">Upcoming Galas</a></li>
-                     <li><a href="http://bucktest-rgu.azurewebsites.net/gala-results.php" role="link">Gala Results</a></li>                
-                     <li><a href="http://bucktest-rgu.azurewebsites.net/galas.php" role="link">View All</a></li>
-                     <li><a href="http://bucktest-rgu.azurewebsites.net/galas/create.php" role="link">Create</a></li>
-                     <li><a href="http://bucktest-rgu.azurewebsites.net/galas/edit.php" role="link">Edit</a></li>
+                     <li><a href="http://localhost/bucktest/upcoming-galas.php" role="link">Upcoming Galas</a></li>
+                     <li><a href="http://localhost/bucktest/gala-results.php" role="link">Gala Results</a></li>                
+                     <li><a href="http://localhost/bucktest/galas.php" role="link">View All</a></li>
+                     <li><a href="http://localhost/bucktest/galas/create.php" role="link">Create</a></li>
+                     <li><a href="http://localhost/bucktest/galas/edit.php" role="link">Edit</a></li>
                   </ul>
                </li>
                <li class="divider"></li>
                <li class="has-dropdown not-click">
-                  <a href="http://bucktest-rgu.azurewebsites.net/members.php" role="link">Members</a> 
+                  <a href="http://localhost/bucktest/members.php" role="link">Members</a> 
                   <ul class="dropdown">
-                     <li><a href="http://bucktest-rgu.azurewebsites.net/members.php" role="link">View All</a></li>
-                     <li><a href="http://bucktest-rgu.azurewebsites.net/members/create.php" role="link">Create</a></li>
-                     <li><a href="http://bucktest-rgu.azurewebsites.net/members/edit.php" role="link">Edit</a></li>
+                     <li><a href="http://localhost/bucktest/members.php" role="link">View All</a></li>
+                     <li><a href="http://localhost/bucktest/members/create.php" role="link">Create</a></li>
+                     <li><a href="http://localhost/bucktest/members/edit.php" role="link">Edit</a></li>
                   </ul>
                </li>
                <li class="divider"></li>
                <li class="has-dropdown not-click">
-                  <a href="http://bucktest-rgu.azurewebsites.net/squads.php" role="link">Squads</a> 
+                  <a href="http://localhost/bucktest/squads.php" role="link">Squads</a> 
                   <ul class="dropdown">
-                     <li><a href="http://bucktest-rgu.azurewebsites.net/squads.php" role="link">View All</a></li>
-                     <li><a href="http://bucktest-rgu.azurewebsites.net/squads/create.php" role="link">Create</a></li>
-                     <li><a href="http://bucktest-rgu.azurewebsites.net/squads/edit.php" role="link">Edit</a></li>
+                     <li><a href="http://localhost/bucktest/squads.php" role="link">View All</a></li>
+                     <li><a href="http://localhost/bucktest/squads/create.php" role="link">Create</a></li>
+                     <li><a href="http://localhost/bucktest/squads/edit.php" role="link">Edit</a></li>
                   </ul>
                </li>
                <li class="divider"></li>
                <li class="has-dropdown not-click">
-                  <a href="http://bucktest-rgu.azurewebsites.net/venues.php" role="link">Venues</a> 
+                  <a href="http://localhost/bucktest/venues.php" role="link">Venues</a> 
                   <ul class="dropdown">
-                     <li><a href="http://bucktest-rgu.azurewebsites.net/venues.php" role="link">View All</a></li>
-                     <li><a href="http://bucktest-rgu.azurewebsites.net/venues/create.php" role="link">Create</a></li>
-                     <li><a href="http://bucktest-rgu.azurewebsites.net/venues/edit.php" role="link">Edit</a></li>
+                     <li><a href="http://localhost/bucktest/venues.php" role="link">View All</a></li>
+                     <li><a href="http://localhost/bucktest/venues/create.php" role="link">Create</a></li>
+                     <li><a href="http://localhost/bucktest/venues/edit.php" role="link">Edit</a></li>
                   </ul>
                </li>
                <li class="divider"></li>
                <li class="has-dropdown not-click">
-                  <a href="http://bucktest-rgu.azurewebsites.net/news.php" role="link">News</a> 
+                  <a href="http://localhost/bucktest/news.php" role="link">News</a> 
                   <ul class="dropdown">
-                     <li><a href="http://bucktest-rgu.azurewebsites.net/news.php" role="link">View All</a></li>
-                     <li><a href="http://bucktest-rgu.azurewebsites.net/news/create.php" role="link">Create</a></li>
-                     <li><a href="http://bucktest-rgu.azurewebsites.net/news/edit.php" role="link">Edit</a></li>
+                     <li><a href="http://localhost/bucktest/news.php" role="link">View All</a></li>
+                     <li><a href="http://localhost/bucktest/news/create.php" role="link">Create</a></li>
+                     <li><a href="http://localhost/bucktest/news/edit.php" role="link">Edit</a></li>
                   </ul>
                </li>
                <li class="divider"></li>
-               <li class="lightgradient"><a href="http://bucktest-rgu.azurewebsites.net/gallery.php" role="link">Gallery</a></li>
+               <li class="lightgradient"><a href="http://localhost/bucktest/gallery.php" role="link">Gallery</a></li>
                <li class="divider"></li>
                <li class="has-dropdown not-click">
-                  <a href="http://bucktest-rgu.azurewebsites.net/shop.php" role="link">Shop</a> 
+                  <a href="http://localhost/bucktest/shop.php" role="link">Shop</a> 
                   <ul class="dropdown">
-                     <li><a href="http://bucktest-rgu.azurewebsites.net/shop.php" role="link">View All</a></li>
-                     <li><a href="http://bucktest-rgu.azurewebsites.net/shop/create.php" role="link">Create</a></li>
-                     <li><a href="http://bucktest-rgu.azurewebsites.net/shop/edit.php" role="link">Edit</a></li>
+                     <li><a href="http://localhost/bucktest/shop.php" role="link">View All</a></li>
+                     <li><a href="http://localhost/bucktest/shop/create.php" role="link">Create</a></li>
+                     <li><a href="http://localhost/bucktest/shop/edit.php" role="link">Edit</a></li>
                   </ul>
                </li>
                <li class="divider"></li>
                <li class="has-dropdown not-click">
-                  <a href="http://bucktest-rgu.azurewebsites.net/about.php" role="link">About</a> 
+                  <a href="http://localhost/bucktest/about.php" role="link">About</a> 
                   <ul class="dropdown">
-                     <li><a href="http://bucktest-rgu.azurewebsites.net/about.php" role="link">About BASC</a></li>
-                     <li><a href="http://bucktest-rgu.azurewebsites.net/about/committee.php" role="link">Committee</a></li>
-                     <li><a href="http://bucktest-rgu.azurewebsites.net/about/timetable.php" role="link">Timetable</a></li>
-                     <li><a href="http://bucktest-rgu.azurewebsites.net/about/club-records.php" role="link">Club Records</a></li>
-                     <li><a href="http://bucktest-rgu.azurewebsites.net/about/join.php" role="link">Join Us</a></li>
+                     <li><a href="http://localhost/bucktest/about.php" role="link">About BASC</a></li>
+                     <li><a href="http://localhost/bucktest/about/committee.php" role="link">Committee</a></li>
+                     <li><a href="http://localhost/bucktest/about/timetable.php" role="link">Timetable</a></li>
+                     <li><a href="http://localhost/bucktest/about/club-records.php" role="link">Club Records</a></li>
+                     <li><a href="http://localhost/bucktest/about/join.php" role="link">Join Us</a></li>
                   </ul>
                </li>
                <li class="divider"></li>
-               <li><a href="http://bucktest-rgu.azurewebsites.net/contact.php" role="link">Contact</a></li>
+               <li><a href="http://localhost/bucktest/contact.php" role="link">Contact</a></li>
                <li class="divider"></li>
-               <li><a href="http://bucktest-rgu.azurewebsites.net/member-area.php">Member Area</a></li> 
+               <li><a href="http://localhost/bucktest/member-area.php">Member Area</a></li> 
                <li class="divider"></li>
-               <li class="has-form"> <a href="http://bucktest-rgu.azurewebsites.net/logout.php" class="small button">Logout</a> </li> 
+               <li class="has-form"> <a href="http://localhost/bucktest/logout.php" class="small button">Logout</a> </li> 
                <li class="divider"></li>
             </ul>
          </div>
@@ -98,30 +98,30 @@
          <div class="top-bar-section">
             <ul class="middle">
                <li class="divider"></li>
-               <li class="lightgradient"><a href="http://bucktest-rgu.azurewebsites.net/upcoming-galas.php" role="link">Upcoming Galas</a></li>
+               <li class="lightgradient"><a href="http://localhost/bucktest/upcoming-galas.php" role="link">Upcoming Galas</a></li>
                <li class="divider"></li>
-               <li class="lightgradient"><a href="http://bucktest-rgu.azurewebsites.net/gala-results.php" role="link">Gala Results</a></li>                
+               <li class="lightgradient"><a href="http://localhost/bucktest/gala-results.php" role="link">Gala Results</a></li>                
                <li class="divider"></li>
-               <li class="lightgradient"><a href="http://bucktest-rgu.azurewebsites.net/news.php" role="link">News</a></li>
+               <li class="lightgradient"><a href="http://localhost/bucktest/news.php" role="link">News</a></li>
                <li class="divider"></li>
-               <li class="lightgradient"><a href="http://bucktest-rgu.azurewebsites.net/gallery.php" role="link">Gallery</a></li>
+               <li class="lightgradient"><a href="http://localhost/bucktest/gallery.php" role="link">Gallery</a></li>
                <li class="divider"></li>
-               <li class="lightgradient"><a href="http://bucktest-rgu.azurewebsites.net/shop.php" role="link">Shop</a></li>
+               <li class="lightgradient"><a href="http://localhost/bucktest/shop.php" role="link">Shop</a></li>
                <li class="divider"></li>
                <li class="has-dropdown not-click">
-                  <a href="http://bucktest-rgu.azurewebsites.net/about.php" role="link">About</a> 
+                  <a href="http://localhost/bucktest/about.php" role="link">About</a> 
                   <ul class="dropdown">
-                     <li><a href="http://bucktest-rgu.azurewebsites.net/about.php" role="link">About BASC</a></li>
-                     <li><a href="http://bucktest-rgu.azurewebsites.net/about/committee.php" role="link">Committee</a></li>
-                     <li><a href="http://bucktest-rgu.azurewebsites.net/about/timetable.php" role="link">Timetable</a></li>
-                     <li><a href="http://bucktest-rgu.azurewebsites.net/about/club-records.php" role="link">Club Records</a></li>
-                     <li><a href="http://bucktest-rgu.azurewebsites.net/about/join.php" role="link">Join Us</a></li>
+                     <li><a href="http://localhost/bucktest/about.php" role="link">About BASC</a></li>
+                     <li><a href="http://localhost/bucktest/about/committee.php" role="link">Committee</a></li>
+                     <li><a href="http://localhost/bucktest/about/timetable.php" role="link">Timetable</a></li>
+                     <li><a href="http://localhost/bucktest/about/club-records.php" role="link">Club Records</a></li>
+                     <li><a href="http://localhost/bucktest/about/join.php" role="link">Join Us</a></li>
                   </ul>
                </li>
                <li class="divider"></li>
-               <li><a href="http://bucktest-rgu.azurewebsites.net/contact.php" role="link">Contact</a></li>
+               <li><a href="http://localhost/bucktest/contact.php" role="link">Contact</a></li>
                <li class="divider"></li>
-               <li class="has-form"> <a href="http://bucktest-rgu.azurewebsites.net/login.php" class="small button">Login</a> </li> 
+               <li class="has-form"> <a href="http://localhost/bucktest/login.php" class="small button">Login</a> </li> 
                <li class="divider"></li>
             </ul>
          </div>
