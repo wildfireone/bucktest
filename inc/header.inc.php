abf80fc003b0ae0aaf4d820b28a8a0f76d02c5f0
Taity-mini <1504693@rgu.ac.uk>
2016-09-06 21:55:41 +0100

    Added showEditLink function *Will show edit link of a particular section if on the correct viewing page

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -1,6 +1,16 @@
 <?php
 //Set domain here as not all pages include the database file (for some reason...)
 $domain='http://bucktest.dev/';
+
+//Show edit link for each section if the url is on the correct viewing page
+function showEditLink($domain,$view, $edit, $parm)
+{
+    if($_SERVER['PHP_SELF'] === $view) {
+        $id = $_GET[$parm];
+        $link = $domain . $edit. '?' . $parm . '=' . $id;
+        echo '<li><a href="'.$link.'" role="link">Edit</a></li>';
+    }
+}
 ?>
 <header>
    <a href="<?php echo $domain ?>index.php" class="nohover">
@@ -8,7 +18,6 @@ $domain='http://bucktest.dev/';
          <h1 class="middle centre">Bucksburn Amateur Swimming Club</h1>
       </div>
    </a>
-
 <?php
    if (isset($_SESSION["username"])) {
       echo '<div class="contain-to-grid header-section">
@@ -23,7 +32,9 @@ $domain='http://bucktest.dev/';
                      <li><a href="'. $domain . 'gala-results.php" role="link">Gala Results</a></li>                
                      <li><a href="'. $domain . 'galas.php" role="link">View All</a></li>
                      <li><a href="'. $domain . 'galas/create.php" role="link">Create</a></li>
-                     <li><a href="'. $domain . 'galas/edit.php" role="link">Edit</a></li>
+                     ';
+                    showEditLink($domain, '/galas/view.php','galas/edit.php', 'id');
+                    echo'
                   </ul>
                </li>
                <li class="divider"></li>
@@ -31,51 +42,51 @@ $domain='http://bucktest.dev/';
                   <a href="'. $domain . 'members.php" role="link">Members</a> 
                   <ul class="dropdown">
                      <li><a href="'. $domain . 'members.php" role="link">View All</a></li>
-                     <li><a href="'. $domain . 'members/create.php" role="link">Create</a></li>
-                     <li><a href="'. $domain . 'members/edit.php" role="link">Edit</a></li>
-                  </ul>
+                     <li><a href="'. $domain . 'members/create.php" role="link">Create</a></li>';
+                     showEditLink($domain, '/members/view.php','members/edit.php', 'id');
+                echo '</ul>
                </li>
                <li class="divider"></li>
                <li class="has-dropdown not-click">
-                  <a href="'. $domain . 'squads.php" role="link">Squads</a> 
+                  <a href="'. $domain . 'squads.php" role="link">Squads</a>
                   <ul class="dropdown">
                      <li><a href="'. $domain . 'squads.php" role="link">View All</a></li>
-                     <li><a href="'. $domain . 'squads/create.php" role="link">Create</a></li>
-                     <li><a href="'. $domain . 'squads/edit.php" role="link">Edit</a></li>
-                  </ul>
+                     <li><a href="'. $domain . 'squads/create.php" role="link">Create</a></li>';
+                      showEditLink($domain, '/squads/view.php','squads/edit.php', 'id');
+                  echo'</ul>
                </li>
                <li class="divider"></li>
                <li class="has-dropdown not-click">
-                  <a href="'. $domain . 'venues.php" role="link">Venues</a> 
+                  <a href="'. $domain . 'venues.php" role="link">Venues</a>
                   <ul class="dropdown">
                      <li><a href="'. $domain . 'venues.php" role="link">View All</a></li>
-                     <li><a href="'. $domain . 'venues/create.php" role="link">Create</a></li>
-                     <li><a href="'. $domain . 'venues/edit.php" role="link">Edit</a></li>
-                  </ul>
+                     <li><a href="'. $domain . 'venues/create.php" role="link">Create</a></li>';
+                     showEditLink($domain, '/venues/view.php','venues/edit.php', 'id');
+                  echo'</ul>
                </li>
                <li class="divider"></li>
                <li class="has-dropdown not-click">
-                  <a href="'. $domain . 'news.php" role="link">News</a> 
+                  <a href="'. $domain . 'news.php" role="link">News</a>
                   <ul class="dropdown">
                      <li><a href="'. $domain . 'news.php" role="link">View All</a></li>
-                     <li><a href="'. $domain . 'news/create.php" role="link">Create</a></li>
-                     <li><a href="'. $domain . 'news/edit.php" role="link">Edit</a></li>
-                  </ul>
+                     <li><a href="'. $domain . 'news/create.php" role="link">Create</a></li>';
+                     showEditLink($domain, '/news/view.php','news/edit.php', 'id');
+                  echo'</ul>
                </li>
                <li class="divider"></li>
                <li class="lightgradient"><a href="'. $domain . 'gallery.php" role="link">Gallery</a></li>
                <li class="divider"></li>
                <li class="has-dropdown not-click">
-                  <a href="'. $domain . 'shop.php" role="link">Shop</a> 
+                  <a href="'. $domain . 'shop.php" role="link">Shop</a>
                   <ul class="dropdown">
                      <li><a href="'. $domain . 'shop.php" role="link">View All</a></li>
-                     <li><a href="'. $domain . 'shop/create.php" role="link">Create</a></li>
-                     <li><a href="'. $domain . 'shop/edit.php" role="link">Edit</a></li>
-                  </ul>
+                     <li><a href="'. $domain . 'shop/create.php" role="link">Create</a></li>';
+                     showEditLink($domain, '/shop/view.php','shop/edit.php', 'id');
+                  echo '</ul>
                </li>
                <li class="divider"></li>
                <li class="has-dropdown not-click">
-                  <a href="'. $domain . 'about.php" role="link">About</a> 
+                  <a href="'. $domain . 'about.php" role="link">About</a>
                   <ul class="dropdown">
                      <li><a href="'. $domain . 'about.php" role="link">About BASC</a></li>
                      <li><a href="'. $domain . 'about/committee.php" role="link">Committee</a></li>
@@ -87,9 +98,9 @@ $domain='http://bucktest.dev/';
                <li class="divider"></li>
                <li><a href="'. $domain . 'contact.php" role="link">Contact</a></li>
                <li class="divider"></li>
-               <li><a href="'. $domain . 'member-area.php">Member Area</a></li> 
+               <li><a href="'. $domain . 'member-area.php">Member Area</a></li>
                <li class="divider"></li>
-               <li class="has-form"> <a href="'. $domain . 'logout.php" class="small button">Logout</a> </li> 
+               <li class="has-form"> <a href="'. $domain . 'logout.php" class="small button">Logout</a> </li>
                <li class="divider"></li>
             </ul>
          </div>
@@ -104,7 +115,7 @@ $domain='http://bucktest.dev/';
                <li class="divider"></li>
                <li class="lightgradient"><a href="'. $domain . 'upcoming-galas.php" role="link">Upcoming Galas</a></li>
                <li class="divider"></li>
-               <li class="lightgradient"><a href="'. $domain . 'gala-results.php" role="link">Gala Results</a></li>                
+               <li class="lightgradient"><a href="'. $domain . 'gala-results.php" role="link">Gala Results</a></li>
                <li class="divider"></li>
                <li class="lightgradient"><a href="'. $domain . 'news.php" role="link">News</a></li>
                <li class="divider"></li>
@@ -113,7 +124,7 @@ $domain='http://bucktest.dev/';
                <li class="lightgradient"><a href="'. $domain . 'shop.php" role="link">Shop</a></li>
                <li class="divider"></li>
                <li class="has-dropdown not-click">
-                  <a href="'. $domain . 'about.php" role="link">About</a> 
+                  <a href="'. $domain . 'about.php" role="link">About</a>
                   <ul class="dropdown">
                      <li><a href="'. $domain . 'about.php" role="link">About BASC</a></li>
                      <li><a href="'. $domain . 'about/committee.php" role="link">Committee</a></li>
@@ -125,7 +136,7 @@ $domain='http://bucktest.dev/';
                <li class="divider"></li>
                <li><a href="'. $domain . 'contact.php" role="link">Contact</a></li>
                <li class="divider"></li>
-               <li class="has-form"> <a href="'. $domain . 'login.php" class="small button">Login</a> </li> 
+               <li class="has-form"> <a href="'. $domain . 'login.php" class="small button">Login</a> </li>
                <li class="divider"></li>
             </ul>
          </div>
