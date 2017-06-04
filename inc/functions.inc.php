e2bc54b857e8304946749d9cdff98184ee554458
Taity-mini <1504693@rgu.ac.uk>
2017-06-04 17:31:43 +0100

    Login Function Fixed *Login SQL query was invalid, now corrected *Allowed anyone to login regardless of password - pretty critical fix! *Removed old MD5 check as well

--- inc/functions.inc.php
+++ inc/functions.inc.php
@@ -1,51 +1,31 @@
 <?php
 
-function login($username, $password, $conn) {
-    
-
-
-    if(strlen($password['password']) == 32) // MD5 hash check
-    {
-        $sql = "SELECT username FROM users WHERE username = :username AND password = :password";
+function login($username, $password, $conn)
+{
+    try {
+        $sql = "SELECT username, password FROM users WHERE username = :username";
         $stmt = $conn->prepare($sql);
         $stmt->bindParam(':username', htmlentities($username), PDO::PARAM_STR);
-        $stmt->bindParam(':password', md5(htmlentities($password)), PDO::PARAM_STR);
+        $stmt->execute();
+        $results = $stmt->fetchAll();
+        $hash = @$results[0]['password'];
 
-        try {
-            $stmt->execute();
-            $results = $stmt->fetchAll();
-            if ($stmt->rowCount() == 1) {
+        if (isset($results)) {
+            if (password_verify($password, $hash)) {
                 return true;
-            }
-        } catch (PDOException $e) {
-            return "Query failed: " . $e->getMessage();
-        }
-
-    } else{
-        try {
-            $sql = "SELECT username FROM users WHERE username = :username AND password = :password";
-            $stmt = $conn->prepare($sql);
-            $stmt->bindParam(':username', htmlentities($username), PDO::PARAM_STR);
-            $stmt->execute();
-            $results = $stmt->fetchAll();
-            $hash = @$results[0]['password'];
-
-            if (isset($results)) {
-                if (password_verify($password, $hash)) {
-                    return true;
-                } else {
-                    return false;
-                }
             } else {
                 return false;
             }
-        } catch (PDOException $e) {
-            return "Login failed: " . $e->getMessage();
+        } else {
+            return false;
         }
+    } catch (PDOException $e) {
+        return "Login failed: " . $e->getMessage();
     }
 }
 
-function isEmailValid($email) {
+function isEmailValid($email)
+{
     if (count($email) <= 250) {
         if (count($email) <> 0) {
             if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
@@ -61,19 +41,20 @@ function isEmailValid($email) {
     }
 }
 
-function addUser($conn, $username, $password) {  
+function addUser($conn, $username, $password)
+{
     $sql = "INSERT INTO users VALUES (:username, :password)";
     $stmt = $conn->prepare($sql);
     $stmt->bindParam(':username', htmlentities($username), PDO::PARAM_STR);
     $stmt->bindParam(':password', md5(htmlentities($password)), PDO::PARAM_STR);
 
-        try {
-            $stmt->execute();
-            $results = $stmt->fetchAll();
-            return true;
-        } catch (PDOException $e) {            
-            return "Query failed: " . $e->getMessage();
-        }             
+    try {
+        $stmt->execute();
+        $results = $stmt->fetchAll();
+        return true;
+    } catch (PDOException $e) {
+        return "Query failed: " . $e->getMessage();
+    }
 }
 
 ?> 
