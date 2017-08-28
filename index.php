ccad6096643e55ebfb7840d893db82ce1f4b202f
Taity-mini <1504693@rgu.ac.uk>
2017-08-28 23:18:22 +0100

    Working Front Page Slide show *Uses slick.js to create auto playing slideshow *Based on photos from a particular gallery id *If gallery is null then uses preset images

--- index.php
+++ index.php
@@ -7,37 +7,85 @@ require 'inc/security.inc.php';
 
 <!DOCTYPE html>
 <html lang="en-GB">
-    
-<head>    
-    <?php include 'inc/meta.inc.php';?>
+
+<head>
+    <?php include 'inc/meta.inc.php'; ?>
     <title>Home | Bucksburn Amateur Swimming Club</title>
     <link href='http://fonts.googleapis.com/css?family=Bree+Serif' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Hind' rel='stylesheet' type='text/css'>
     <link href="css/site.css" rel="stylesheet"/>
 </head>
 
-<body>   
-    <?php include 'inc/header.inc.php'; ?>   
-    <br>
-    <div class="row" id="content">
-        <div class="large-12 columns centre">
-            <img src="img/banner.jpg" alt=""/>
+<body>
+<?php include 'inc/header.inc.php'; ?>
+<br>
+<div class="row" id="content">
+    <div class="large-12 columns centre">
+        <?php
 
-            <?php
+        require 'obj/news.obj.php';
+        require 'obj/galas.obj.php';
+        require 'obj/venues.obj.php';
+        require 'obj/gallery_albums.obj.php';
+        require 'obj/gallery_photos.obj.php';
 
-                require 'obj/news.obj.php';
-                require 'obj/galas.obj.php';
-                require 'obj/venues.obj.php';
 
+        $conn = dbConnect();
+        $newsItem = new News();
+        $newsList = $newsItem->getMostRecent($conn, 3);
+        $gala = new Galas();
+        $galaList = $gala->listAllCompletedGalas($conn, 2);
+        $venue = new Venues();
 
-                $conn = dbConnect();
-                $newsItem = new News();
-                $newsList = $newsItem->getMostRecent($conn,3);
-                $gala = new Galas();
-                $galaList = $gala->listAllCompletedGalas($conn,2);
-                $venue = new Venues();
+        //         PHOTO GALLERY CODE - START
+        $albums = new gallery_album("6");
+        if ($albums->doesExist($conn)) {
+            $photos = new gallery_photos();
+            $photos->setAlbumID($albums->getAlbumID());
+            $photoList = $photos->listPhotoAlbum($conn);
+        } else {
+            $photos = null;
+            $photoList = null;
+        }
 
-                echo '<div class="row">
+        echo '<div class="sc">';
+        if ($photoList != null) {
+
+            $count = 0;
+            $max = sizeof($photoList);
+
+            foreach ($photoList as $photos) {
+                $photo = new gallery_photos();
+                $photo->setPhotoID($photos["photoID"]);
+                $photo->getAllDetails($conn);
+                $count += $count;
+                echo '          <div>
+                                <img width="500px" height="341px" src="' . $photo->getFullFilePath() . '"/>
+                                </div>
+                            ';
+            }
+            echo "</div>";
+        } else {
+            echo '
+                    <div>
+                        <img src="img/slider/slider_1.png"/>
+                    </div>
+                   <div>
+                        <img src="img/slider/slider_2.png"/>
+                    </div>
+                    <div>
+                        <img src="img/slider/slider_3.png"/>
+                    </div>
+                     <div>
+                        <img src="img/slider/slider_4.png"/>
+                    </div>
+               </div>
+        
+                      ';
+        }
+
+        //         PHOTO GALLERY CODE END
+        echo '<div class="row">
                 <div class="large-6 medium-6 small-12 columns index">
                     <h2 class="h3 capitalise">About Us</h2>
                     <p>Bucksburn Amateur Swimming Club (BASC) is a fairly young club with over 100 swimmers, which hosts a wide range of age groups and abilities. BASC was formed in 1990, and at that time had only 1 core aim - to encourage local participation in the sport of swimming.</p>
@@ -46,54 +94,53 @@ require 'inc/security.inc.php';
                     </div>
                     </div>';
 
-                echo '<div class="large-6 medium-6 small-12 columns index">
+        echo '<div class="large-6 medium-6 small-12 columns index">
                     <h2 class="h3 capitalise">Recent Gala Results</h2>
                     <div class="row">';
 
-                foreach ($galaList as $galaItem) {
-                    $gala->setID($galaItem["id"]);
-                    $gala->getAllDetails($conn);
-                    $venue->setID($galaItem["venueID"]);
-                    $venue->getAllDetails($conn);
-                    //echo "<p>Gala ID: ".$gala->getID()." Venue ID: ".$gala->getVenueID()."</p>";
-                    $link = "gala-results/view.php?id=" . $gala->getID();
+        foreach ($galaList as $galaItem) {
+            $gala->setID($galaItem["id"]);
+            $gala->getAllDetails($conn);
+            $venue->setID($galaItem["venueID"]);
+            $venue->getAllDetails($conn);
+            $link = "gala-results/view.php?id=" . $gala->getID();
 
-                    echo '<div class="large-12 medium-12 small-12 columns centre">
+            echo '<div class="large-12 medium-12 small-12 columns centre">
                             <h3 class="h4 capitalise"><a href="' . $link . '">' . $gala->getTitle() . '</a></h3>
                             <h4 class="h6"><i>' . $venue->getVenue() . ' - ' . date("d/m/Y", strtotime($gala->getDate())) . '</i></h4>
                     </div>';
-                }
-                echo '</div><div class="large-6 large-centered middle-12 small-12 columns">
+        }
+        echo '</div><div class="large-6 large-centered middle-12 small-12 columns">
                     <a href="gala-results.php" class="button small radius h6 capitalise margin">View all Gala Results</a>
                     </div></div></div><hr>';
 
-                echo '<div class="large-12 medium-12 small-12 columns index">
+        echo '<div class="large-12 medium-12 small-12 columns index">
                     <h2 class="h3 capitalise">Recent News</h2>
                     <div class="row">';
 
-                foreach ($newsList as $news) {
-                    $newsItem->setID($news["id"]);
-                    $newsItem->getAllDetails($conn);
-                    $link = "news/view.php?id=" . $newsItem->getID();
+        foreach ($newsList as $news) {
+            $newsItem->setID($news["id"]);
+            $newsItem->getAllDetails($conn);
+            $link = "news/view.php?id=" . $newsItem->getID();
 
-                    echo '<div class="large-4 medium-4 small-12 columns centre">
+            echo '<div class="large-4 medium-4 small-12 columns centre">
                         <article>
                             <h3 class="h4 capitalise"><a href="' . $link . '">' . $newsItem->getTitle() . '</a></h3>
                             <h4 class="h6"><i>' . date("l jS F Y", strtotime($newsItem->getDate())) . '</i></h4>
                             <p>' . $newsItem->getSummary(100) . '</p>
                         </article>
                     </div>';
-                }
-                echo '</div>
+        }
+        echo '</div>
                 <div class="large-3 large-centered middle-6 small-12 columns">
                     <a href="news.php" class="button small radius h6 capitalise">View all Recent News</a>
                 </div>
                 </div>';
-            ?>
+        ?>
 
-        </div>
     </div>
-    <?php include 'inc/footer.inc.php';?>
+</div>
+<?php include 'inc/footer.inc.php'; ?>
 </body>
 
 </html>
